bdec506477d52d813bcec5a38c88a564ec90fe17
Andre Vicente <isquicha@gmail.com>
2021-04-10 01:47:18 -0300

    feat(front): Update list view to list all emails

--- front_end/src/views/GoogleListEmails.vue
+++ front_end/src/views/GoogleListEmails.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>Google List Emails</h1>
+  <ul>
+    <li v-for="email in emailList" :key="email">
+      {{ email }}
+    </li>
+  </ul>
 </template>
 
 
@@ -10,26 +15,50 @@ interface IConnections {
   [index: number]: {
     resourceName: string;
     etag: string;
-    emailAddresses?: string;
+    emailAddresses?: {
+      [index: number]: {
+        value: string;
+      };
+    };
   };
   [Symbol.iterator](): any;
 }
 
 export default defineComponent({
   name: "GoogleListEmails",
+  data: () => {
+    let data: { emailList: [string] | [] } = {
+      emailList: [],
+    };
+    return data;
+  },
+  computed: {},
+
   async mounted() {
     let uri = "https://people.googleapis.com";
     uri += "/v1/people/me/connections?personFields=emailAddresses";
     uri += "&access_token=" + this.$route.params.token;
-    uri += "&page_size=" + 100;
-    const response = await fetch(uri);
-    const json = await response.json();
+    uri += "&page_size=" + 1000;
 
-    const connections: IConnections = json.connections;
-    for (let connection of connections) {
-      if (!connection.emailAddresses) continue;
-      console.log(connection.emailAddresses);
-    }
+    let nextPageToken = null;
+    do {
+      let localUri = uri;
+      if (nextPageToken) localUri += "&pageToken=" + nextPageToken;
+
+      const response = await fetch(localUri);
+      const json = await response.json();
+      nextPageToken = json.nextPageToken;
+      const connections: IConnections = json.connections;
+
+      for (let connection of connections) {
+        const emails = connection.emailAddresses;
+        if (!emails) continue;
+        for (let email of emails) {
+          // ? Insert the email at the end of the emailList
+          this.emailList.splice(this.emailList.length, 0, email.value);
+        }
+      }
+    } while (nextPageToken);
   },
 });
 </script>
