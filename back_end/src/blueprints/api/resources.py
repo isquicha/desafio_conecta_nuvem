8bd3ec78e3c11332feaa5425be4eb367d0176a91
Andre Vicente <isquicha@gmail.com>
2021-04-12 20:50:48 -0300

    feat(back): Update user api add post method

--- back_end/src/blueprints/api/resources.py
+++ back_end/src/blueprints/api/resources.py
@@ -1,6 +1,36 @@
+from flask import request
 from flask.views import MethodView
+
+from src.utils import json_response
+from src.extensions.authentication import create_user, AlreadyRegisteredError
 
 
 class UserAPI(MethodView):
     def post(self):
-        pass
+        body = request.get_json()
+        if body is None:
+            return json_response(
+                message="A JSON body must be provided", status_code=400
+            )
+
+        username = body.get("username", None)
+        password = body.get("password", None)
+
+        if username is None:
+            return json_response(
+                message="Field 'username' must not be empty", status_code=400
+            )
+        if password is None:
+            return json_response(
+                message="Field 'password' must not be empty", status_code=400
+            )
+
+        try:
+            create_user(username=username, password=password)
+            return json_response(
+                message=f"Created user {username}", status_code=201
+            )
+        except AlreadyRegisteredError:
+            return json_response(
+                message="Username already registered", status_code=400
+            )
