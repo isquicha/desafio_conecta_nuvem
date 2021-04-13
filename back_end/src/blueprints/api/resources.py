7e8640bc0d78bc8874877bd1fb5ac17506fbb9d7
Andre Vicente <isquicha@gmail.com>
2021-04-12 23:17:22 -0300

    feat(back): Add path in responses

--- back_end/src/blueprints/api/resources.py
+++ back_end/src/blueprints/api/resources.py
@@ -10,7 +10,9 @@ class UserAPI(MethodView):
         body = request.get_json()
         if body is None:
             return json_response(
-                message="A JSON body must be provided", status_code=400
+                message="A JSON body must be provided",
+                status_code=400,
+                path=request.full_path,
             )
 
         username = body.get("username", None)
@@ -18,19 +20,27 @@ class UserAPI(MethodView):
 
         if username is None:
             return json_response(
-                message="Field 'username' must not be empty", status_code=400
+                message="Field 'username' must not be empty",
+                status_code=400,
+                path=request.full_path,
             )
         if password is None:
             return json_response(
-                message="Field 'password' must not be empty", status_code=400
+                message="Field 'password' must not be empty",
+                status_code=400,
+                path=request.full_path,
             )
 
         try:
             create_user(username=username, password=password)
             return json_response(
-                message=f"Created user {username}", status_code=201
+                message=f"Created user {username}",
+                status_code=201,
+                path=request.full_path,
             )
         except AlreadyRegisteredError:
             return json_response(
-                message="Username already registered", status_code=400
+                message="Username already registered",
+                status_code=400,
+                path=request.full_path,
             )
